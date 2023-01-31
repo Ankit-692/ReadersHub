42949ac4c0c31fca66fd4150aa03427868708390
Ankit-692 <bhartiankit692@gmail.com>
2023-01-31 14:11:05 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.10.5 install -r requirements.txt
-python3.10.5 manage.py collectstatic
+pip install -r requirements.txt
+python manage.py collectstatic
 echo " BUILD END"
